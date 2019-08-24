eb3b8b092fcac7599f49b6343cc807397c8f0848
Exr0n <25516241+Exr0n@users.noreply.github.com>
2019-08-24 11:03:15 -0700

    role creation, colors functional

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /* 
 
 alot of content taken from https://gist.github.com/eslachance/3349734a98d30011bb202f47342601d3
+todo: role position: it doesn't go to the top??
 
 */
 
@@ -13,10 +14,13 @@ let m = {
 };
 
 let client = new m.Discord.Client();
-
+const talkedRecently = new Set();
 
 const handle = {
-  "help": async (msg) => { /* todo */ },
+  "help": (msg) => {
+    return msg.reply(`Try \`${conf.ux.prefix}set <hexcode>\` to change your role color!`)
+    .catch(console.error);
+  },
   "ping": async (msg) => {
     const m = await msg.channel.send("Ping?");
     m.edit(`Pong! Latency is ${m.createdTimestamp - msg.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
@@ -24,6 +28,49 @@ const handle = {
   },
   "invite": async (msg) => {
     return msg.channel.send(`Invite me to your server with ${conf.ux.invite}`);
+  },
+  "set": async (msg, hex) => {
+    if (msg.guild === undefined) return msg.reply(`Must be in guild!`);
+    
+    if (hex.startsWith('#')) hex = hex.substr(1);
+    if (hex === "000000") hex = "010101";
+    
+    let torem = msg.member.roles.find(r => /^hexify\-[0-9A-F]{6}$/i.test(r.name));
+    if (torem !== null)
+      msg.member.removeRole(torem)
+      .then(() => {
+        if (torem.members.size === 0) { // empty
+          return torem.delete();
+        }
+      })
+      .catch(() => msg.reply("Unable to remove previous color!"))
+      .catch(console.error);
+    
+    if (/^[0-9A-F]{6}$/i.test(hex)) {
+      let toadd = msg.guild.roles.find(r => r.name === conf.fn.role_prefix + hex);
+      if (toadd === null) {
+        toadd = await msg.guild.createRole({
+          name: conf.fn.role_prefix + hex,
+          color: "#" + hex,
+          position: 1000
+        })
+        .catch(() => msg.reply(`Unable to create new role!`))
+        .catch(console.error);
+      }
+      
+      console.log(`${Date()} adding role ${toadd.name} to ${msg.author.tag}`);
+      
+      return msg.member.addRole(toadd)
+      .then(
+        () => msg.reply(`Your role color was set to #${hex}!`)
+        .catch(console.error),
+        () => msg.reply(`Unable to add role!`)
+        .catch(console.error)
+      );
+    } else {
+      return msg.reply(`Invalid hex code!`)
+      .catch(console.error);
+    }
   }
 }
 
@@ -31,19 +78,33 @@ const handle = {
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag} at ${Date()}`);
   
-  client.user.setActivity(`for ${conf.ux.prefix}`, { type: 'WATCHING' });
+  client.user.setActivity(`for ${conf.ux.prefix}help`, { type: 'WATCHING' });
 });
 
 client.on('message', async message => {
   if (message.author.bot) return; // ignore bots
   if (! message.content.startsWith(conf.ux.prefix)) return; // ignore things that don't start with our prefix
   
+  if (talkedRecently.has(message.author.id)) { // set message cooldown
+    return message.reply("Please wait before trying again!");
+  }
+  talkedRecently.add(message.author.id);
+  setTimeout(() => {
+    talkedRecently.delete(message.author.id);
+  }, conf.ux.cooldown);
+    
   const args = message.content.slice(conf.ux.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   
   if (command in handle) {
-    await handle[command](message, ...args);
+    return handle[command](message, ...args)
+      .catch(() => message.reply("Something went wrong!"))
+      .catch(console.error);
+  } else {
+    return message.reply(`Unkown command! Try \`${conf.ux.prefix}help..\`.`)
+      .catch(console.error);
   }
+  
 });
 
 client.login(require("./#ignore/token.json")).catch(console.error);
