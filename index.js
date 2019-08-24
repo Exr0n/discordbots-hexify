377f746d6d101bd1338eb7804aa634298f9009ba
Exr0n <25516241+Exr0n@users.noreply.github.com>
2019-08-24 11:16:16 -0700

    update help message

--- index.js
+++ index.js
@@ -17,59 +17,76 @@ let client = new m.Discord.Client();
 const talkedRecently = new Set();
 
 const handle = {
-  "help": (msg) => {
-    return msg.reply(`Try \`${conf.ux.prefix}set <hexcode>\` to change your role color!`)
-    .catch(console.error);
-  },
-  "ping": async (msg) => {
-    const m = await msg.channel.send("Ping?");
-    m.edit(`Pong! Latency is ${m.createdTimestamp - msg.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-    return m;
-  },
-  "invite": async (msg) => {
-    return msg.channel.send(`Invite me to your server with ${conf.ux.invite}`);
-  },
-  "set": async (msg, hex) => {
-    if (msg.guild === undefined) return msg.reply(`Must be in guild!`);
-    
-    if (hex.startsWith('#')) hex = hex.substr(1);
-    if (hex === "000000") hex = "010101";
-    
-    let torem = msg.member.roles.find(r => /^hexify\-[0-9A-F]{6}$/i.test(r.name));
-    if (torem !== null)
-      msg.member.removeRole(torem)
-      .then(() => {
-        if (torem.members.size === 0) { // empty
-          return torem.delete();
-        }
-      })
-      .catch(() => msg.reply("Unable to remove previous color!"))
+  "help": {
+    desc: "Displays this message",
+    run: async (msg) => {
+      let cmdlist = "";
+      for (k of handle) {
+        cmdlist += `${k.padEnd(20)}${handle[k].run}\n`;
+      }
+      return msg.reply(`Try \`${conf.ux.prefix}set <hexcode>\` to change your role color!\n${cmdlist}`)
       .catch(console.error);
-    
-    if (/^[0-9A-F]{6}$/i.test(hex)) {
-      let toadd = msg.guild.roles.find(r => r.name === conf.fn.role_prefix + hex);
-      if (toadd === null) {
-        toadd = await msg.guild.createRole({
-          name: conf.fn.role_prefix + hex,
-          color: "#" + hex,
-          position: 1000
+    }
+  },
+  "ping": {
+    run: async (msg) => {
+      const m = await msg.channel.send("Ping?");
+      m.edit(`Pong! Latency is ${m.createdTimestamp - msg.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+      return m;
+    },
+    desc: "Ping the bot"
+  },
+  "invite": {
+    desc: "Get invite link for the bot",
+    run: async (msg) => {
+    return msg.channel.send(`Invite me to your server with ${conf.ux.invite}`);
+    }
+  },
+  "set": {
+    desc: "Set hex color: usage set <6-digit-hex>",
+    run: async (msg, hex) => {
+      if (msg.guild === undefined) return msg.reply(`Must be in guild!`);
+      
+      if (hex.startsWith('#')) hex = hex.substr(1);
+      if (hex === "000000") hex = "010101";
+      
+      let torem = msg.member.roles.find(r => /^hexify\-[0-9A-F]{6}$/i.test(r.name));
+      if (torem !== null) {
+        msg.member.removeRole(torem)
+        .then(() => {
+          if (torem.members.size === 0) { // empty
+            return torem.delete();
+          }
         })
-        .catch(() => msg.reply(`Unable to create new role!`))
+        .catch(() => msg.reply("Unable to remove previous color!"))
         .catch(console.error);
       }
       
-      console.log(`${Date()} adding role ${toadd.name} to ${msg.author.tag}`);
-      
-      return msg.member.addRole(toadd)
-      .then(
-        () => msg.reply(`Your role color was set to #${hex}!`)
-        .catch(console.error),
-        () => msg.reply(`Unable to add role!`)
-        .catch(console.error)
-      );
-    } else {
-      return msg.reply(`Invalid hex code!`)
-      .catch(console.error);
+      if (/^[0-9A-F]{6}$/i.test(hex)) {
+        let toadd = msg.guild.roles.find(r => r.name === conf.fn.role_prefix + hex);
+        if (toadd === null) {
+          toadd = await msg.guild.createRole({
+            name: conf.fn.role_prefix + hex,
+            color: "#" + hex,
+            position: 1000
+          })
+          .catch(() => msg.reply(`Unable to create new role!`))
+          .catch(console.error);
+        }
+        
+        console.log(`${Date()} adding role ${toadd.name} to ${msg.author.tag}`);
+        
+        return msg.member.addRole(toadd)
+        .then(
+          () => msg.reply(`Your role color was set to #${hex}!`)
+          .catch(console.error),
+          () => msg.reply(`Unable to add role!`)
+          .catch(console.error)
+        );
+      } else {
+        return msg.reply(`Invalid hex code!`)
+        .catch(console.error);
+      }
     }
   }
 }
@@ -97,7 +114,7 @@ client.on('message', async message => {
   const command = args.shift().toLowerCase();
   
   if (command in handle) {
-    return handle[command](message, ...args)
+    return handle[command].run(message, ...args)
       .catch(() => message.reply("Something went wrong!"))
       .catch(console.error);
   } else {
